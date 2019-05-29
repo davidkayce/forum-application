0fce22adbd6993d2c243fa043f6cd38add09b36f
davidkayce <david@kora.network>
2019-05-30 00:48:47 +0100

    update: updated dependencies and scripts

--- routes/notes.js
+++ routes/notes.js
@@ -11,9 +11,9 @@ const loadNotes = () => {
   }
 }
 
-const addNotes = (data) => {
-  const notes = loadNotes()
-  const duplicate = notes.find((note) => note.title === data.title) // Find rather than filter here 
+const addNotes = async ({ title }) => { // destructured the object "data" that is meant to be passed here to get the "title" property
+  const notes = await loadNotes()
+  const duplicate = notes.find((note) => note.title === title) // Find rather than filter here 
   if (!duplicate) {
     notes.push(data)
     fs.writeFileSync('notes.json', JSON.stringify(notes)) // Overwrites what was there before
@@ -23,15 +23,15 @@ const addNotes = (data) => {
   }
 }
 
-const deleteNote = (title) => {
-  const notes = loadNotes()
+const deleteNote = async (title) => {
+  const notes = await loadNotes()
   const keptNotes = notes.filter((note) => note.title !== title)
   fs.writeFileSync('notes.json', JSON.stringify(keptNotes))
   return notes.length > keptNotes.length? 'You deleted a note with title: ' + title : 'There is no note with ths title'
 }
 
 notes.get('/', async ctx => {
-  ctx.body = loadNotes()
+  ctx.body = await loadNotes()
 })
 
 notes.get('/:title', async ctx => {
@@ -42,7 +42,7 @@ notes.get('/:title', async ctx => {
   } else {
     try {
       const title = ctx.params.title
-      const notes = loadNotes()
+      const notes = await loadNotes()
       ctx.body = notes.find((note) => note.title === title)
     } catch (error) {
       ctx.body = 'error: ' + error
@@ -50,7 +50,7 @@ notes.get('/:title', async ctx => {
   }
 })
 
-notes.post('/:title', async ctx => {
+notes.post('/:title',  async ctx => {
   if ( !ctx.request.body.content || !ctx.params.title || !ctx.request.body.author) { 
     ctx.body = {
       error: 'You are either not providing a proper author, title or message' 
@@ -63,7 +63,7 @@ notes.post('/:title', async ctx => {
         content: ctx.request.body.content
       }
       console.log(ctx.request.body)
-      ctx.body = addNotes(data)
+      ctx.body = await addNotes(data)
     } catch (error) {
       ctx.body = 'error: ' + error
     }
@@ -78,12 +78,25 @@ notes.delete('/:title', async ctx => {
   } else {
     try {
       const title = ctx.params.title
-      ctx.body = deleteNote(title)
+      ctx.body = await deleteNote(title)
     } catch (error) {
       ctx.body = 'error: ' + error
     }
   }
 })
+
+
+// Note using fetch api for client-side js
+
+// const fetchSomething = async (url) => {
+//   try {
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     console.log(data)
+//   } catch (err) {
+//     console.log(err)
+//   }
+// }
 
 
 module.exports = notes 
