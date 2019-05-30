903622e75a1d83d325ca3c8aa48d8796114077c5
davidkayce <david@kora.network>
2019-05-30 01:59:57 +0100

    update: added proper commenting to app

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 const Koa = require('koa-plus')
-const router = require('./routes')
-const request = require('koa-http-request')
-const { ApolloServer, gql } = require('apollo-server-koa')
+const router = require('./routes') // Import routes folder 
+const request = require('koa-http-request') // Koa library for making http request on the server
+const { ApolloServer, gql } = require('apollo-server-koa') // Getting the apollo library for koa
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
-  }
-`;
+  }`
  
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
   },
-};
+}
  
-const server = new ApolloServer({ typeDefs, resolvers });
- 
+const server = new ApolloServer({ typeDefs, resolvers }) 
 const app = new Koa({
   // Configuration for koa-plus
   body: {
@@ -47,18 +45,17 @@ const app = new Koa({
   }
 })
 
-server.applyMiddleware({ app });
+server.applyMiddleware({ app }) // Wrap the koa server within the apollo server 
  
 async function start () {
-  const port = process.env.PORT || 2400
+  const port = process.env.PORT || 2400 // Set appropriate port based on env variables
 
-  // koa request
-  app.use(request({
-    json: true, //automatically parsing of JSON response
+  app.use(request({ // koa request
+    json: true, //automatic parsing of JSON response
     timeout: 3000,    //3s timeout
   }));
 
-  app.use(router.routes())
+  app.use(router.routes()) // make use of exported routes in the route folder 
 
   app.listen ({ port }, () => {
     console.log(`🚀 Server ready at http://localhost:` + port + `${server.graphqlPath}`)
