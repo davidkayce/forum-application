cf6a369e18361bb76a57209695e5cb00b429100f
davidkayce <david@kora.network>
2019-05-31 17:07:02 +0100

    feat: added serverless configuration

--- graphQL/modules/auth/src/app.js
+++ graphQL/modules/auth/src/app.js
@@ -1,8 +1,28 @@
-const { ApolloServer } = require('apollo-server-lambda');
+const { ApolloServer } = require('apollo-server-lambda')
 const typeDefs = require('./types')
 const resolvers = require('./resolvers')
 
+// To read information about the current request from the API Gateway event  
+// (HTTP headers, HTTP method, body, path, ...) or the current Lambda Context
+// (Function Name, Function Version, awsRequestId, time remaining, ...), use the options function.
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  })
+})
 
-exports.graphqlHandler = server.createHandler();
+// To enable CORS response for requests with credentials 
+// (cookies, http authentication), the allow origin and credentials 
+// header must be set to true.
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true,
+  }
+})
