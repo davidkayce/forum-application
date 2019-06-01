4883277bcc0fb96161389e2ff41f687c2ce330ea
davidkayce <david@kora.network>
2019-06-01 11:16:02 +0100

    feat: switched to koa-plus

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const Koa = require('koa')
-const bodyParser = require('koa-body')
-// require('./database')
+const Koa = require('koa-plus')
 
+require('./database')
 const router = require('./routes') // Import routes folder 
-const app = new Koa()
+
+const app = new Koa({
+  body: {
+    jsonLimit: '10kb' // Sets the json request body limit to 10k
+  },
+  compress: {
+    threshold: 2048 // Sets the threshold to Gzip responses at 2k (2048 bytes)
+  },
+  cors: {
+    origin: '*' // Set the `Access-Control-Allow-Origin` header to be `*`
+  },
+  debug: {
+    name: 'worker' // Set the debug logger name
+  },
+  helmet: {
+    noCache: true,  // Sets the `Cache-Control` headers to prevent caching
+    frameguard: {
+      action: 'deny' // Set the `X-Frame-Options' header to be `DENY`
+    }
+  },
+  json: {
+    pretty: false // Disables pretty-printing
+  },
+  logger: {
+    format: 'dev' // Use the `dev` format of logging
+  }
+})
 
 async function start () {
   const port = process.env.PORT || 2400 // Set appropriate port based on env variables
-
-  app.use(bodyParser)
 
   app.use(router.routes()) // make use of exported routes in the route folder 
 
